8422986fab6bcdc67551086c60346435602abbd5
Pratikshya Pandey <pratikshya.pp19@gmail.com>
2019-03-15 03:03:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
+require 'pry'
 def unsafe?(speed)
-
+if speed < 40 
+true
+elseif speed > 60
+  true
+else
+  false
+binding.pry  
 end
-
-
+end
 
 def not_safe?(speed)
-	
+	speed < 40 ? || speed > 60 true : false
+binding.pry
 end
-	
+
 
 
